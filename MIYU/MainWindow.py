11ff64699d387cd9a146033fce9dded6d92f0616
Robert Clayton <rclayton@theia.io>
2019-08-26 15:02:01 -0700

    Tried doing things with QMovie.

--- MIYU/MainWindow.py
+++ MIYU/MainWindow.py
@@ -1,14 +1,14 @@
 import os
 import requests
-from PySide2.QtCore import Qt, QItemSelectionModel
+from PySide2.QtCore import Qt, QItemSelectionModel, QByteArray, QBuffer, QIODevice
 from PySide2.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QListView, QApplication
-from PySide2.QtGui import QPixmap, QStandardItemModel, QStandardItem
+from PySide2.QtGui import QPixmap, QStandardItemModel, QStandardItem, QMovie
 from MIYU.Yankers import randomImgurResponse, randomImgurResponses
 
 class Image:
     url = Qt.DisplayRole
     image = Qt.UserRole+1
-    data = Qt.UserRole+2
+    response = Qt.UserRole+2
 
 class MainWindow(QFrame):
     def __init__(self):
@@ -32,6 +32,16 @@ class MainWindow(QFrame):
     def _updateViewer(self, selected, deselected):
         # Updates viewer
         index = selected.indexes()[0]
+        response = index.data(role=Image.response)
+        if not index.data(role=Image.image):
+            pixmap = self._makeImage(response)
+            self._linkHistoryModel.setData(index, pixmap, role=Image.image)
+        
+        # if response.headers['Content-Type'] == 'image/gif': #TODO: Find out why QMovie crashes with start()
+        #     movie = index.data(role=Image.image)
+        #     self._viewer.setMovie(movie)
+        #     movie.start()
+        # else:
         self._viewer.setPixmap(index.data(role=Image.image))
 
     def _saveImage(self, response=None):
@@ -41,7 +51,7 @@ class MainWindow(QFrame):
         else:
             item = self._linkHistoryView.selectionModel().selectedIndexes()[0]
             name = item.data(role=Image.url).rsplit('/', 1)[-1]
-            data = item.data(role=Image.data)
+            data = item.data(role=Image.response).content
         saveDirectory = './images'
         if not os.path.exists(saveDirectory):
             os.makedirs(saveDirectory)
@@ -49,11 +59,19 @@ class MainWindow(QFrame):
         with open(saveDirectory, 'wb') as f:
             f.write(data)
 
-    def _makeItem(self, response, maxWidth=700, maxHeight=700):
-        # Creates an image item from the given url
+    def _makeImage(self, response, maxWidth=700, maxHeight=700):
+        # Makes an image for the given response
+        # if response.headers['Content-Type'] == 'image/gif':
+        #     byteArray = QByteArray(response.content)
+        #     buffer = QBuffer(byteArray)
+        #     buffer.open(QIODevice.ReadOnly)
+        #     mov = QMovie(buffer, b'GIF')
+        #     mov.setCacheMode(QMovie.CacheAll)
+        #     mov.setSpeed(100)
+        #     return mov
+
         pixmap = QPixmap()
         pixmap.loadFromData(response.content)
-
         width, height = pixmap.width(), pixmap.height()
         if width > height and width > maxWidth:
             dx = maxWidth / width
@@ -66,26 +84,30 @@ class MainWindow(QFrame):
         elif height == width:
             width = height = maxWidth
         scaled = pixmap.scaled(width, height, transformMode=Qt.SmoothTransformation)
-        
-        # Make item
+
+        return scaled
+
+    def _makeItem(self, response, defer=False):
+        # Creates an image item from the given response
         item = QStandardItem()
         item.setData(response.url, role=Image.url)
-        item.setData(response.content, role=Image.data)
-        item.setData(scaled, role=Image.image)
+        item.setData(response, role=Image.response)
+        if not defer:
+            item.setData(self._makeImage(response), role=Image.image)
         
         return item
 
-    def _newImage(self, response):
+    def _newImage(self, response, defer=False):
         # Creates a new image item, adds it to the model, then updates the view
-        item = self._makeItem(response)
+        item = self._makeItem(response, defer=defer)
         self._linkHistoryModel.appendRow(item)
         self._linkHistoryView.selectionModel().select(self._linkHistoryModel.indexFromItem(item), QItemSelectionModel.ClearAndSelect)
 
         return item
 
-    def mouseReleaseEvent(self, event):
+    def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            self._newImage(randomImgurResponse())
+            self._newImage(randomImgurResponse(), defer=True)
         elif event.button() == Qt.RightButton:
             self._saveImage()
 
